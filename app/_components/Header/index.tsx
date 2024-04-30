d3346c6f7fd66bf3d46d8689b275d1ac51602939
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-04-30 00:03:44 -0300

    chore: add initial header component

---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        width={100}
+        height={30}
+        className="cursor-pointer"
+      />
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
