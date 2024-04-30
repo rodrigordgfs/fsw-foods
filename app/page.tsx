76d86958fee04d827281e6a22a7cecbcb01fa797
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-04-30 00:14:28 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./_components/Header";
+import Search from "./_components/Search";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default Home;
